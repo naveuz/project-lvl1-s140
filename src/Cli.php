22dbff567bb18d09e5fdcdc2848e1b52b8a5ba56
naveuz <az.gost@gmail.com>
2017-08-02 19:57:37 +0300

    change Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -4,9 +4,25 @@ namespace BrainGames\Cli;
 
 use function \cli\line;
 
-function run()
+function run($greeting, $getQuestion, $getAnswer)
 {
-    line('Welcome to the Brain Game!');
-    $name = \cli\prompt('May I have your name?');
+    line("Welcome to the Brain Game!");
+    line($greeting);
+
+    $name = \cli\prompt("May I have your name?");
     line("Hello, %s!", $name);
+
+    for ($i = 0; $i < 3; $i++) {
+        $question = $getQuestion();
+        $rightAnswer = $getAnswer($question);
+        line("Question: %s", $question);
+        $userAnswer = \cli\prompt("Your answer");
+        if ($userAnswer == $rightAnswer) {
+            line("Correct!");
+        } else {
+            return line("'{$userAnswer}' is wrong answer ;(. Correct answer was '{$rightAnswer}'.
+                  Let's try again, {$name}");
+        }
+    }
+    return line("Congratulations, {$name}!");
 }
