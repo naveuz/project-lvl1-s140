deacf908c1a000ab64847cdd7508b9dbb8e5af67
naveuz <az.gost@gmail.com>
2017-08-02 21:48:25 +0300

    add Calc.php

---
+++ src/games/Calc.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace BrainGames\Calc;
+
+use function BrainGames\Cli\run;
+
+function game()
+{
+    $greeting = "What is the result of the expression?";
+
+    $operators = ['+', '-', '*'];
+    $getRandomOperator = function () use ($operators) {
+        return $operators[rand(0, count($operators) - 1)];
+    };
+
+    $getQuestion = function () use ($getRandomOperator) {
+        $num1 = rand(1, 100);
+        $num2 = rand(1, 100);
+        $operator = $getRandomOperator();
+        return "{$num1} {$operator} {$num2}";
+    };
+
+    $getAnswer = function ($str) {
+        $arr = explode(" ", $str);
+        $operator = $arr[1];
+        $num1 = $arr[0];
+        $num2 = $arr[2];
+
+        switch ($operator) {
+            case '+':
+                return $num1 + $num2;
+                break;
+            case '-':
+                return $num1 - $num2;
+                break;
+            case '*':
+                return $num1 * $num2;
+                break;
+            default:
+                break;
+        }
+    };
+
+    run($greeting, $getQuestion, $getAnswer);
+}
