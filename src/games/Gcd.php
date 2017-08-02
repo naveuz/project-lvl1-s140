4332ad0a8486f8dfff91730eabedf1a76738f474
naveuz <az.gost@gmail.com>
2017-08-02 23:26:38 +0300

    add Gcd.php

---
+++ src/games/Gcd.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace BrainGames\Gcd;
+
+use function BrainGames\Cli\run;
+
+function game()
+{
+    $greeting = "Find the greatest common divisor of given numbers.";
+
+    $getQuestion = function () {
+        $num1 = rand(1, 100);
+        $num2 = rand(1, 100);
+        return "{$num1} {$num2}";
+    };
+
+    $getAnswer = function ($str) {
+        $arr = explode(" ", $str);
+        $num1 = $arr[0];
+        $num2 = $arr[1];
+        return gcd($num1, $num2);
+    };
+
+    run($greeting, $getQuestion, $getAnswer);
+}
+
+function gcd($num1, $num2)
+{
+    $min = min($num1, $num2);
+    $max = max($num1, $num2);
+    if ($min === $max || $min === 0) {
+        return $max;
+    }
+    $iter = function ($acc) use (&$iter, $max, $min) {
+        if ($max % $acc === 0 & $min % $acc === 0) {
+            return $acc;
+        }
+        return $iter($acc - 1);
+    };
+    return $iter($min);
+}
