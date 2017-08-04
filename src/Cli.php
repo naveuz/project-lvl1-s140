5af61187714529792e58bc795185d9c89a8997e3
naveuz <az.gost@gmail.com>
2017-08-04 20:26:42 +0300

    add game menu

--- src/Cli.php
+++ src/Cli.php
@@ -6,12 +6,12 @@ use function \cli\line;
 
 function run($greeting, $getQuestion, $getAnswer)
 {
-    line("Welcome to the Brain Game!");
     line($greeting);
-
+    line();
     $name = \cli\prompt("May I have your name?");
     line("Hello, %s!", $name);
-
+    line();
+    
     for ($i = 0; $i < 3; $i++) {
         $question = $getQuestion();
         $rightAnswer = $getAnswer($question);
@@ -25,4 +25,39 @@ function run($greeting, $getQuestion, $getAnswer)
         }
     }
     return line("Congratulations, {$name}!");
+}
+
+function startGame()
+{
+      line("********************************");
+      line("***Welcome to the Brain Game!***");
+      line("********************************");
+      line();
+      line("List of games:");
+      line("1. brain-even");
+      line("2. brain-calc");
+      line("3. brain-gcd");
+      line("4. brain-balance");
+      line("5. brain-progression");
+      line("6. brain-prime");
+      line();
+      $number = \cli\prompt("Select game");
+      line();
+
+    switch ($number) {
+        case '1':
+            return \BrainGames\Even\game();
+        case '2':
+            return \BrainGames\Calc\game();
+        case '3':
+            return \BrainGames\Gcd\game();
+        case '4':
+            return \BrainGames\Balance\game();
+        case '5':
+            return \BrainGames\Progression\game();
+        case '6':
+            return \BrainGames\Prime\game();
+        default:
+            return "Game not found";
+    }
 }
